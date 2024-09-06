2796fb60a24b991017066f5a7845b55f4dd49b72
narbi5h <narbi5h@users.noreply.github.com>
2024-09-05 21:10:29 -0700

    BANA 607 Sep 5

---
+++ Test.py
@@ -0,0 +1,10 @@
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+
+x=int(input("Enter a number: "))
+y=int(input("Enter another number: "))
+result=x*y
+print("The result of ",x," multiplied by ",y," is ",result)
+print("Hello", first_name, last_name)
+
+
---
+++ sample.py
@@ -0,0 +1,7 @@
+x = int(input("Enter a number: "))
+
+y = int(input("Enter another number: "))
+
+result = x * y
+
+print("The result of ", x "multiplied by",y, "is", result)
