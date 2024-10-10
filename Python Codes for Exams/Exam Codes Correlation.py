e2f59df307206f1ec27fea03b798751111002814
narbi5h <narbi5h@yahoo.com>
2024-10-10 16:19:18 -0700

    commit

--- Python Codes for Exams/Exam Codes Correlation.py
+++ Python Codes for Exams/Exam Codes Correlation.py
@@ -1,19 +1,19 @@
-# import numpy as np
-# import pandas as pd
-# # Load data
-# housing_df = pd.read_csv('WestRoxbury.csv') # importing the csv file
-# #Renaming Columns
-# housing_df.columns = [s.strip().replace(' ', '_') for s in housing_df.columns] 
-# all columns
-# # Descriptive statistics
-# print(housing_df.describe()) # show summary statistics for each column
-# df = pd.DataFrame(housing_df)
-# print(df)
-# #Finding Correlation
-# correlation_matrix = df.corr()
-# print(correlation_matrix)
-# # Visualize the finding
-# import seaborn as sn
-# import matplotlib.pyplot as plt
-# sn.heatmap(correlation_matrix, annot=True)
-# plt.show()
+import numpy as np
+import pandas as pd
+# Load data
+housing_df = pd.read_csv('WestRoxbury.csv') # importing the csv file
+#Renaming Columns
+housing_df.columns = [s.strip().replace(' ', '_') for s in housing_df.columns] 
+all columns
+# Descriptive statistics
+print(housing_df.describe()) # show summary statistics for each column
+df = pd.DataFrame(housing_df)
+print(df)
+#Finding Correlation
+correlation_matrix = df.corr()
+print(correlation_matrix)
+# Visualize the finding
+import seaborn as sn
+import matplotlib.pyplot as plt
+sn.heatmap(correlation_matrix, annot=True)
+plt.show()
