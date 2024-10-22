69649440f75a289fbf2206c092a4740a1a833b2e
narbi5h <narbi5h@users.noreply.github.com>
2024-10-21 19:03:56 -0700

    backtrack

---
+++ CORRELATION/Correlation.py
@@ -0,0 +1,48 @@
+import numpy as np
+import pandas as pd
+
+
+
+# Load data
+golf_df = pd.read_csv('golf.csv') # importing the csv file
+
+print(golf_df.head())
+
+# Remove 'Rank' and 'Player' columns
+golf_df = golf_df.drop(columns=['Rank', 'Player'])
+
+
+#Renaming Columns
+golf_df.columns = [s.strip().replace(' ', '_') for s in golf_df.columns] # all columns
+# Remove dollar signs and commas from 'Earnings' column and convert to numeric
+golf_df['Earnings'] = golf_df['Earnings'].replace({'\$': '', ',': ''}, regex=True).astype(float)
+
+
+
+# Print the skew of all the columns
+df_filled_mean = golf_df.fillna(golf_df.mean(numeric_only=True))
+
+
+
+
+
+
+# Descriptive statistics
+#print(housing_df.describe()) # show summary statistics for each column
+
+
+df = pd.DataFrame(df_filled_mean)
+#print(df)
+
+#Finding Correlation
+correlation_matrix = df.corr()
+correlation_matrix.to_csv('golf_correlation_matrix.csv')
+
+
+# Visualize the finding
+import seaborn as sn
+import matplotlib.pyplot as plt
+
+sn.heatmap(correlation_matrix, annot=True)
+plt.show()
+
